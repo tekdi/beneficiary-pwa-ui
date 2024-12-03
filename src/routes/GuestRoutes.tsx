4fdb318444fb74d53fb5d96082e600df6689ca20
sagar <sagarkoshti1990@gmila.com>
2024-12-03 14:30:39 +0530

    fix: add routes

--- src/routes/GuestRoutes.tsx
+++ src/routes/GuestRoutes.tsx
@@ -2,11 +2,21 @@ import { lazy } from "react";
 
 const Splash = lazy(() => import("../screens/auth/Splash"));
 const Signup = lazy(() => import("../screens/auth/SignUp"));
+const ExploreBenefits = lazy(() => import("../screens/benefit/Benefits"));
+const BenefitsDetails = lazy(() => import("../screens/benefit/Details"));
 
 const routes = [
   {
     path: "/signup",
     component: Signup,
+  },
+  {
+    path: "/explorebenefits",
+    component: ExploreBenefits,
+  },
+  {
+    path: "/benefits/:id",
+    component: BenefitsDetails,
   },
   {
     path: "*",
