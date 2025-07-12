585d781b89536bb7b98d87d605679fa35099ad9f
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-07-12 19:31:25 +0530

    Task #241515 feat: Remove unused MenuItem import from Header component

--- src/components/common/admin/Header.tsx
+++ src/components/common/admin/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, HStack, MenuItem, Text, Toast } from '@chakra-ui/react';
+import { Box, HStack, Text, Toast } from '@chakra-ui/react';
 import Logo from '../../../assets/images/logo.png';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { logoutUser } from '../../../services/auth/auth';
