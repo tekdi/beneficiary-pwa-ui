38177dd41ad3ad2f7297d5cb4cc39a0a82558e03
Vidya Hanumant Khandekar <vidya.khandekar@tekditechnologies.com>
2025-07-14 21:52:06 +0530

    Task #243453: Beneficiary - UI  handle getOne API response on user profile screen (#175)

--- src/screens/UserProfile.tsx
+++ src/screens/UserProfile.tsx
@@ -102,7 +102,15 @@ const UserProfile: React.FC = () => {
 					</Text>
 				</Flex>
 
-				<UserDetails userData={{ ...userData }} />
+				<UserDetails
+					userData={{
+						firstName: userData?.firstName,
+						middleName: userData?.middleName,
+						lastName: userData?.lastName,
+						dob: userData?.dob,
+						customFields: userData?.customFields || [],
+					}}
+				/>
 				<Box
 					p={5}
 					shadow="md"
@@ -116,7 +124,7 @@ const UserProfile: React.FC = () => {
 							userDocuments={userData?.docs}
 						/>
 						{showIframe ? (
-							<UploadDocumentEwallet/>
+							<UploadDocumentEwallet />
 						) : (
 							<CommonButton
 								onClick={() => setShowIframe(true)}
