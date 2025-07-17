160afac76c604d11d747461ee1ca9207fd6c0687
Vidya Hanumant Khandekar <vidya.khandekar@tekditechnologies.com>
2025-07-17 14:36:51 +0530

    Admin Panel: Add hardcoded core fields to FieldMappingConfig and combine with fetched API fields  (#183)
    Co-authored-by: Manoj L <manojLondhe@users.noreply.github.com>

--- src/screens/admin/FieldMappingConfig.tsx
+++ src/screens/admin/FieldMappingConfig.tsx
@@ -85,7 +85,41 @@ const FieldMappingConfig: React.FC<FieldMappingConfigProps> = ({
 	const fetchFields = async () => {
 		try {
 			const apiFields = await fetchFieldsAPI('USERS', 'User');
-			setFields(apiFields);
+
+			// Add hardcoded fields
+			const coreFields = [
+				{
+					fieldId: 'coreField-firstName',
+					label: 'First Name',
+					name: 'firstName',
+					type: 'text',
+					isRequired: true,
+				},
+				{
+					fieldId: 'coreField-middleName',
+					label: 'Middle Name',
+					name: 'middleName',
+					type: 'text',
+					isRequired: true,
+				},
+				{
+					fieldId: 'coreField-lastName',
+					label: 'Last Name',
+					name: 'lastName',
+					type: 'text',
+					isRequired: true,
+				},
+				{
+					fieldId: 'coreField-dob',
+					label: 'Date of Birth',
+					name: 'dob',
+					type: 'date',
+					isRequired: true,
+				},
+			];
+
+			// Combine API fields with hardcoded fields
+			setFields([...coreFields, ...apiFields]);
 		} catch (error) {
 			console.error('Error fetching fields:', error);
 			toast({
